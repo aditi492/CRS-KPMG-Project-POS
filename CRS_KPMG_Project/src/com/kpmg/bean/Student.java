82084fe2097d18166bcb1a03835aa09c5ba5c415
Aditi Roy <aditiroy492@gmail.com>
2023-04-26 12:14:13 +0530

    Initial changes for student

---
+++ CRS_KPMG_Project/src/com/kpmg/bean/Student.java
@@ -0,0 +1,90 @@
+package com.kpmg.bean;
+import java.util.ArrayList;
+
+public class Student {
+	
+	public String firstName;
+	public String lastName;
+	public String username;
+	public String password;
+	
+	//Define constructors
+	public Student() {
+			
+	}
+	
+	public static ArrayList<String> registeredCourses = new ArrayList<String>();
+	/**
+	 * @return the registeredCourses
+	 */
+	public static ArrayList<String> getRegisteredCourses() {
+		return registeredCourses;
+	}
+
+	/**
+	 * @param registeredCourses the registeredCourses to set
+	 */
+	public static void setRegisteredCourses(ArrayList<String> registeredCourses) {
+		Student.registeredCourses = registeredCourses;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+}
+
